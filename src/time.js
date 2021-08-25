07dd6c3ab80adc0897a53a3d2ace92b33b3e5df2
Jccarcher <archerjcc@hotmail.com>
2021-08-25 18:45:28 -0300

    se agregan nuevos paquetes y zona horaria

--- src/time.js
+++ src/time.js
@@ -1,12 +1,13 @@
 //variables
 const express = require('express');
 const app = express();
-
 //package
 const m = require('moment');
+const tz = require('moment-timezone');
 
-var currentDate = m();
-var futureMonth = m(currentDate).add(1, 'M');
+var timezone = m.tz.guess(); //pedir la zona horaria 
+var currentDate = m(); //pedir la fecha actual
+var futureMonth = m(currentDate).add(1, 'M'); //pedir la fecha futura 
 var backMonth = m(currentDate).add(-1,'M')
 var futureMonthEnd = m(futureMonth).endOf('month');
 
@@ -14,9 +15,6 @@ if(currentDate.date() != futureMonth.date() && futureMonth.isSame(futureMonthEnd
     futureMonth = futureMonth.add(1, 'd');
 }
 
-if(currentDate.date() != futureMonth.date() && futureMonth.isSame(futureMonthEnd.format('YYYY-MM-DD'))) {
-    futureMonth = futureMonth.add(-1, 'd');
-}
-
-console.log("Tiempo Actual",currentDate);
-console.log("Mes Futuro",futureMonth);
+console.log("Tiempo Actual",currentDate); //imprimo la fecha actual 
+console.log("Mes Futuro",futureMonth); //imprimo la fecha futura 
+console.log("Time zone", timezone); //imprimo la zona horaria
